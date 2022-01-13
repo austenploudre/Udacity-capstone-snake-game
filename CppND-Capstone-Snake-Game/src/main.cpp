452d65b23667f1ba95173f9a54f74d1a87e0a8a9
Austen Ploudre <60715092+austenploudre@users.noreply.github.com>
2022-01-13 11:07:37 -0800

    Update main.cpp

--- CppND-Capstone-Snake-Game/src/main.cpp
+++ CppND-Capstone-Snake-Game/src/main.cpp
@@ -2,6 +2,7 @@
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
+#include <chrono>
 
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
@@ -15,13 +16,19 @@ int main() {
   Controller controller;
   Game game(kGridWidth, kGridHeight);
   game.SetPlayerName();
-  game.UpdateDifficulty();
+  //game.UpdateDifficulty();
+  //start game clock
+  auto start = std::chrono::system_clock::now();  
   game.Run(controller, renderer, kMsPerFrame);
   //game.SetPlayerGameTime();
+  //end game clock
+  auto end = std::chrono::system_clock::now();
+  //calculate time elapsed
+  auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Name: " << game.GetPlayerName() << "\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
-  //std::cout << "Elapsed Time: " << game.GetPlayerGameTime() << "\n";
+  std::cout << "Elapsed Time: " << duration.count() << "seconds" << "\n";
   return 0;
 }
